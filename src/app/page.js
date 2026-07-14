d4aec0d89ccacf3156472415ee15673817852e59
habipyilmaz <habipyilmaztr@gmail.com>
2026-07-15 01:54:02 +0300

    SELAM EDELM

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    <div>
+    <div>  
       
-       <h1>RUKİYE SENA YILMAZ 111</h1>
+       <h1>RUKİYE SENA YILMAZ 111 </h1>
       <h1>HAYDİ BİSMİLLAH</h1>
+
+
       
-    </div>
+    </div><h1>HAYDİ BİSMİLLAH</h1>
     </div>
   );
 }
