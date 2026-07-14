ac0c6f327cfdbd004d55f727138aafbfbc70acba
habipyilmaz <habipyilmaztr@gmail.com>
2026-07-15 02:16:57 +0300

    ÇOK ŞÜKÜRLER OLSUN

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     <div> 
-       <h1>ELHAMDÜLİLLAH</h1>
+       <h1>ELHAMDÜLİLLAH HAMD OLSUN</h1>
  
 
 
