c4e5b576f427aa719285ace474158c7047cf8001
habipyilmaz <habipyilmaztr@gmail.com>
2026-07-15 01:50:22 +0300

    test deploy

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     <div>
       
-       <h1>RUKİYE SENA YILMAZ</h1>
+       <h1>RUKİYE SENA YILMAZ 111</h1>
       <h1>HAYDİ BİSMİLLAH</h1>
       
     </div>
