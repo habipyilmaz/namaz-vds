5f92ce399ed87f6e63b38de3c463057274db78cf
habipyilmaz <habipyilmaztr@gmail.com>
2026-07-15 02:08:12 +0300

    değişiklik açıklaması

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,11 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     <div>  
-      
-       <h1>RUKİYE SENA YILMAZ 111 </h1>
-      <h1>HAYDİ BİSMİLLAH</h1>
+ 
 
 
       
-    </div><h1>HAYDİ BİSMİLLAH</h1>
+    </div><h1>HAYDİ BİSMİLLAH - ŞÜKÜR</h1>
     </div>
   );
 }
