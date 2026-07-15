3fe97b4a9ee4a25e0eb471ef18ab5544e2b81824
habipyilmaz <habipyilmaztr@gmail.com>
2026-07-15 11:02:20 +0300

    deploy remote testi

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,15 +4,14 @@ import "./globals.css";
  
 
 export const metadata = {
-  title: "Namaz Rehberi Sitesi",
+  title: "Namaz Rehberi Sitesi- V.15.07.2026",
   description: "Namaz Rehberi - SAV - 2 ",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="tr" 
-    >
+      lang="tr"     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,9 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     <div> 
+
+             <h1>15 TEMMUZ 2026</h1>
        <h1>ELHAMDÜLİLLAH HAMD OLSUN</h1>
- 
-
-
       
     </div>
     </div>
