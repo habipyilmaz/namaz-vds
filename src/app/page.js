c92843157b5b5406ce94d64ab5d699d183cc5e9f
habipyilmaz <habipyilmaztr@gmail.com>
2026-07-15 11:43:33 +0300

    RUK-MAY

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,9 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     <div> 
 
-             <h1>15 TEMMUZ 2026</h1>
+        <h1>15 TEMMUZ 2026</h1>
        <h1>ELHAMDÜLİLLAH HAMD OLSUN</h1>
-        <h1>Allahuteala yol selameti versin inşallah - SÜBHANELLEZİ</h1>
+        <h1>RUKİYE VE MUHAMMED ALİ VERSİYONU</h1>
       
     </div>
     </div>
